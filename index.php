7e50f04451dbabd6b771fbfe79ea89b73c1aff4f
Phongsathorn Phuttha <60310544@go.buu.ac.th>
2020-02-03 16:34:09 +0700

    Update index.php
    update my name xD

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php 
 
-    echo "gitTEST";
-    echo "<br>";
-    echo "gitTEST 2";
-    echo "<br>";
-    echo "Hello Thailand";
-    echo "<br>";
-    echo "aik";
-    echo "<br>";
-    echo "Manussawee";
-    echo "<br>";
-
-    echo "nuu";
-    echo "<br>";
-    echo "Katechai";
-    echo "<br>";
-    echo "ไก่เอาไหมกรุง";
-    echo "<br>";
-    echo "gitDevelop";
+    echo "Phongsathorn Phuttha <br>";
 
 ?>
